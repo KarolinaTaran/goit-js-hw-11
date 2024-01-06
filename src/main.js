6c524e5c81b7f0bf4b0b7352798297612e73f1ac
karolina taran <karolina.taran@gmail.com>
2024-01-06 14:46:56 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,94 +5,73 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const apiKey = '41588863-34d6c5c01e7f0c7c667666520';
 
-const searchInput = document.getElementById('search-input');
-const imageContainer = document.getElementById('image-container');
+const searchInput = document.querySelector('#search-input');
+const imageContainer = document.querySelector('#image-container');
 const loader = document.querySelector('.loader');
+const form = document.querySelector('#search-form');
+const lightbox = new SimpleLightbox('#image-container a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-document
-  .getElementById('search-form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault();
-    const query = searchInput.value.trim();
+form.addEventListener('submit', onFormSubmit);
 
-    if (!query) {
-      clearImages();
-      return;
-    }
+function onFormSubmit(event) {
+  event.preventDefault();
+  const query = searchInput.value.trim();
 
-    loader.style.display = 'block';
-    const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+  if (!query) {
+    clearImages();
+    return;
+  }
 
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        loader.style.display = 'none';
-        if (Array.isArray(data.hits) && data.hits.length > 0) {
-          renderImages(data.hits);
-          searchInput.value = '';
-        } else {
-          clearImages();
-          iziToast.show({
-            message:
-              'Sorry, there are no images matching your search query. Please try again!',
-            messageColor: 'white',
-            backgroundColor: 'red',
-            position: 'topRight',
-          });
-          searchInput.value = '';
-        }
-      })
-      .catch(() => {
-        loader.style.display = 'none';
-        console.error('Error!');
-      });
-  });
+  loader.style.display = 'block';
+  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      loader.style.display = 'none';
+      if (Array.isArray(data.hits) && data.hits.length > 0) {
+        renderImages(data.hits);
+        lightbox.refresh();
+        searchInput.value = '';
+      } else {
+        clearImages();
+        iziToast.show({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: 'white',
+          backgroundColor: 'red',
+          position: 'topRight',
+        });
+        searchInput.value = '';
+      }
+    })
+    .catch(() => {
+      loader.style.display = 'none';
+      console.error('Error!');
+    });
+}
 
 function renderImages(images) {
   clearImages();
-
-  images.forEach(image => {
-    const imageCard = document.createElement('div');
-    imageCard.classList.add('image-card');
-
-    const imageLink = document.createElement('a');
-    imageLink.href = image.largeImageURL;
-
-    const imageElement = document.createElement('img');
-    imageElement.src = image.webformatURL;
-    imageElement.alt = image.tags;
-
-    const imageInfo = document.createElement('div');
-    imageInfo.classList.add('image-info');
-
-    const likes = document.createElement('p');
-    likes.innerHTML = `<strong>Likes:</strong> ${image.likes}`;
-
-    const views = document.createElement('p');
-    views.innerHTML = `<strong>Views:</strong> ${image.views}`;
-
-    const comments = document.createElement('p');
-    comments.innerHTML = `<strong>Comments:</strong> ${image.comments}`;
-
-    const downloads = document.createElement('p');
-    downloads.innerHTML = `<strong>Downloads:</strong> ${image.downloads}`;
-
-    imageInfo.appendChild(likes);
-    imageInfo.appendChild(likes);
-    imageInfo.appendChild(views);
-    imageInfo.appendChild(comments);
-    imageInfo.appendChild(downloads);
-
-    imageLink.appendChild(imageElement);
-    imageLink.appendChild(imageInfo);
-    imageCard.appendChild(imageLink);
-    imageContainer.appendChild(imageCard);
+  images.map(image => {
+    const imageCard = `
+      <div class="image-card">
+        <a href="${image.largeImageURL}">
+          <img src="${image.webformatURL}" alt="${image.tags}">
+          <div class="image-info">
+            <p><strong>Likes:</strong> ${image.likes}</p>
+            <p><strong>Views:</strong> ${image.views}</p>
+            <p><strong>Comments:</strong> ${image.comments}</p>
+            <p><strong>Downloads:</strong> ${image.downloads}</p>
+          </div>
+        </a>
+      </div>
+    `;
+    imageContainer.innerHTML += imageCard;
   });
-  const lightbox = new SimpleLightbox('#image-container a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
-  lightbox.refresh();
 }
 
 function clearImages() {
